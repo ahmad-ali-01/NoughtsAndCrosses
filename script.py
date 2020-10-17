fa66d00228c08851cb808243a33956f2c43e426f
ahmad-ali-01 <ahmad-ali-01@outlook.com>
2020-10-17 19:21:23 +0100

    Fixing the style of board

--- script.py
+++ script.py
@@ -10,9 +10,15 @@ class Board:
         self.board[3-y][x-1] = "X"
 
     def display(self):
+        print("\n")
         for i in range(3):
             if i < 2:
-                print(" | ".join(self.board[i]))
-                print("---------")
+                print("  " + " | ".join(self.board[i]))
+                print("  " + "---------")
             else:
-                print(" | ".join(self.board[i]))
+                print("  "+" | ".join(self.board[i]))
+        print("\n")
+board = Board()
+board.nought(1,1)
+board.cross(2,2)
+board.display()
