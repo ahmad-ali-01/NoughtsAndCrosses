d3dcef5c21df1142c5b52a65174d0a9a49a40696
ahmad-ali-01 <ahmad-ali-01@outlook.com>
2020-10-17 19:50:13 +0100

    Commenting...

--- script.py
+++ script.py
@@ -4,12 +4,15 @@ class Board:
         self.board = [[" " for i in range(3)] for i in range(3)]
 
     def nought(self, x:int, y:int):
+        # Places a nought on the board.
         self.board[3-y][x-1] = "O"
 
     def cross(self, x:int, y:int):
+        # Places a cross on the board.
         self.board[3-y][x-1] = "X"
 
     def display(self):
+        # Displays the board.
         print("\n")
         for i in range(3):
             if i < 2:
@@ -18,7 +21,14 @@ class Board:
             else:
                 print("  "+" | ".join(self.board[i]))
         print("\n")
+
+    def solved(self):
+        # Returns true if the game has been decided otherwise false.
+        self.boolboard = [[True if self.board[j][i] == "O" else False if self.board[j][i] == "X" else None for i in range(3)] for j in range(3)]
+        return self.boolboard
+
 board = Board()
 board.nought(1,1)
 board.cross(2,2)
 board.display()
+print(board.solved())
